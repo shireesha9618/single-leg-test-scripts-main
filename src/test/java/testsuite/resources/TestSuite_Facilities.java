07d11b2a4eaf0440c4da89a6c282acab268e8a0d
jagadeesh <jagadeesh@qtsolv.com>
2022-11-27 10:38:59 +0530

    removed unnecessary code

--- src/test/java/testsuite/resources/TestSuite_Facilities.java
+++ src/test/java/testsuite/resources/TestSuite_Facilities.java
@@ -14,17 +14,5 @@ import org.openqa.selenium.interactions.Actions;
 import java.sql.DriverAction;
 
 public class TestSuite_Facilities {
-    private final Locator skip_Btn = Locator.builder().withWeb(By.cssSelector(".productfruits--btn.productfruits--card-footer-skip-button"));
 
-    public void click_Skip_Btn() {
-        ActionHelper.waitForLoaderToHide();
-//        JavascriptExecutor js = (JavascriptExecutor) DriverManager.getDriver();
-//        Actions builder = new Actions(DriverManager.getDriver());
-//        if(isPresent_Skip_Btn())
-////          js.executeScript("arguments[0].click();", skip_Btn);
-//            builder
-//                    .moveToElement((WebElement) skip_Btn).click().build();
-//    }
-
-    }
 }
